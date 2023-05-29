30b75fde556f202d23d48fb7cc6b1e24240f67f6
ikram ezzirague <ikram.elzirague2002@gmail.com>
2023-05-29 12:42:01 +0200

    version 1 back-end

--- app.py
+++ app.py
@@ -4,26 +4,51 @@ import json
 
 app = Flask(__name__)
 CORS(app)
-# Définir une route pour le login
+
+users = []  # Liste des utilisateurs enregistrés
+
+# Route pour le login
 @app.route('/login', methods=['POST'])
 def login():
     # Récupérer les données de login depuis la requête POST
-    data = request.get_data()
-    data = json.loads(data)
-    print(data)
+    data = request.get_json()
     email = data.get('email')
     password = data.get('password')
 
     # Vérifier les informations de login
-    if email == 'test@example.com' and password == 'password':
-        # Si les informations de login sont correctes, renvoyer une réponse JSON avec un message de succès
-        response = {'status': 'success', 'message': 'Login réussi !'}
-        return (response)
-    else:
-        # Si les informations de login sont incorrectes, renvoyer une réponse JSON avec un message d'erreur
-        response = {'status': 'error', 'message': 'Nom d\'utilisateur ou mot de passe invalide.'}
-        return (response)
-    
+    for user in users:
+        if user['email'] == email and user['password'] == password:
+            # Si les informations de login sont correctes, renvoyer une réponse JSON avec un message de succès
+            response = {'status': 'success', 'message': 'Login réussi !'}
+            return (response)
+
+    # Si les informations de login sont incorrectes, renvoyer une réponse JSON avec un message d'erreur
+    response = {'status': 'error', 'message': 'Nom d\'utilisateur ou mot de passe invalide.'}
+    return (response)
+
+# Route pour le sign up
+@app.route('/signup', methods=['POST'])
+def signup():
+    # Récupérer les données d'inscription depuis la requête POST
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+
+    # Vérifier si l'utilisateur existe déjà
+    for user in users:
+        if user['email'] == email:
+            response = {'status': 'error', 'message': 'Cet utilisateur existe déjà.'}
+            return (response)
+
+    # Ajouter l'utilisateur à la liste des utilisateurs enregistrés
+    user = {'email': email, 'password': password, 'first_name': first_name, 'last_name': last_name}
+    users.append(user)
+
+    # Renvoyer une réponse JSON avec un message de succès
+    response = {'status': 'success', 'message': 'Inscription réussie !'}
+    return (response)
 
 
 if __name__ == '__main__':
