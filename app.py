c2b9943bb1aaf4e945670bf759179f128290ed4c
HIBAJANNANE <hibajannane2016@gmail.com>
2023-06-04 11:31:04 +0100

    signup with cors error

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS,cross_origin
 from flask_mysqldb import MySQL
 
 
-
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={"/*": {"origins": "http://localhost:3000"}})
